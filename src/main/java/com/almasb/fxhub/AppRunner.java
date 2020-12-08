329c7698cc7d26cfd526ff3127b757650903e808
AlmasB <almaslvl@gmail.com>
2020-12-08 11:10:36 +0000

    tmp fix for mac

--- src/main/java/com/almasb/fxhub/AppRunner.java
+++ src/main/java/com/almasb/fxhub/AppRunner.java
@@ -2,9 +2,13 @@ package com.almasb.fxhub;
 
 // Adapted from https://mkyong.com/java/java-how-to-run-windows-bat-file/
 
+import com.almasb.fxgl.core.util.Platform;
+import com.almasb.fxgl.dsl.FXGL;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
@@ -24,6 +28,16 @@ public class AppRunner {
         if (fileName.endsWith(".msi")) {
             processBuilder = new ProcessBuilder("msiexec", "/i", fileName);
         } else {
+            // TODO: mac hardcoded
+            if (FXGL.getSettings().getPlatform() == Platform.MAC) {
+                try {
+                    new ProcessBuilder("chmod", "+x", fileName).start().waitFor();
+                    new ProcessBuilder("chmod", "+x", Paths.get(fileName).resolveSibling("java").toAbsolutePath().toString()).start().waitFor();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
             // this handles .bat
             processBuilder = new ProcessBuilder(fileName);
         }
